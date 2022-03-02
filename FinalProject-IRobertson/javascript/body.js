681448d5b2108b6bf890e142eedc4490b79a83da
Ian Robertson <isrobertson21@gmail.com>
2022-03-02 14:35:38 -0500

    adjusted body javascript tags and js file

--- FinalProject-IRobertson/javascript/body.js
+++ FinalProject-IRobertson/javascript/body.js
@@ -1,1 +1,14 @@
 // simple javascript for adding text between pages
+const pTag = ["<p>", "</p>"];
+const liTag = ["<li>", "</li>"];
+
+var footer = document.getElementById("footer");
+var stringBuilder = [];
+
+// sets text between all footers
+function setFooters()
+{
+    footer.innerHTML = "<p>" + "Hello Footer" + "</p>";
+}
+
+setFooters();
