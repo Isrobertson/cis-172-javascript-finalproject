730367b40fd7d90d1c64b742a6eaa950eec47c28
Ian Robertson <isrobertson21@gmail.com>
2022-03-05 16:25:24 -0500

    added script to load for navbar and footer

--- FinalProject-IRobertson/javascript/body.js
+++ FinalProject-IRobertson/javascript/body.js
@@ -1,5 +1,21 @@
 // simple javascript for adding text between pages
-var footer = document.getElementById("footer");
+var footer = document.getElementsByTagName("footer")[0];
+var navbarList = ["home.html", "athletic.html", "game.html", "contact_me.html"];
+var navbarContent = ["Home", "Athletic", "Game", "Contact Me"];
+
+// function adds each li child node to ul tag
+function setNavbar()
+{
+    var navbar = document.getElementsByClassName("navbar")[0];
+    var listContent;
+
+    for(var i = 0; i < navbarList.length; i++)
+    {
+        listContent = document.createElement("li"); 
+        listContent.innerHTML = "<a href=" + navbarList[i] + ">" + navbarContent[i] + "</a>";
+        navbar.appendChild(listContent);
+    }
+}
 
 // sets text between all footers
 function setFooters()
@@ -7,4 +23,14 @@ function setFooters()
     footer.innerHTML = "website by Ian S. Robertson";
 }
 
-setFooters();
+//setNavbar();
+//setFooters();
+
+// page loader handler for functions
+if(window.addEventListener) {
+    window.addEventListener("load", setNavbar, false);
+    window.addEventListener("load", setFooters, false);
+} else if(window.attachEvent) {
+    window.attachEvent("onload", setNavbar);
+    window.attachEvent("onload", setFooters);
+}
