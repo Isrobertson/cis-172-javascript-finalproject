16f3a337aa9eca65a5afc5d52b867bc136a3cd62
Ian Robertson <isrobertson21@gmail.com>
2022-03-06 22:10:47 -0500

    added javascript date and time funciton

--- FinalProject-IRobertson/javascript/body.js
+++ FinalProject-IRobertson/javascript/body.js
@@ -1,4 +1,6 @@
 // simple javascript for adding text between pages
+
+var currentDate = new Date();
 var footer = document.getElementsByTagName("footer")[0];
 var navbarList = ["home.html", "athletic.html", "game.html", "contact_me.html"];
 var navbarContent = ["Home", "Athletic", "Game", "Contact Me"];
@@ -17,10 +19,33 @@ function setNavbar()
     }
 }
 
-// sets text between all footers
+function getCurrentDate()
+{
+    var cDay = currentDate.getDate();
+    var cMonth = currentDate.getMonth() + 1;
+    var cYear = currentDate.getFullYear();
+
+    return "Date: " + cMonth + "/" + cDay + "/" + cYear;
+}
+
+function getCurrentTime()
+{
+    var time = currentDate.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true
+    });
+
+    return time;
+}
+
+// gets date and time. sets text between all footers
 function setFooters()
 {
-    footer.innerHTML = "Website by: Ian Robertson";
+    var buildFooter = ["", ""];
+    buildFooter[0] = getCurrentDate();
+    buildFooter[1] = getCurrentTime();
+    footer.innerHTML = "<b>" + buildFooter[0] + "<br>" + buildFooter[1] + "<b>";
 }
 
 // page loader handler for functions
