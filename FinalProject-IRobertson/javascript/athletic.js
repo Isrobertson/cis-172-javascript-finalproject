301f0af4772a74e39ed8b3d2753a0d6065f10638
Ian Robertson <isrobertson21@gmail.com>
2022-04-10 13:30:27 -0400

    added javascript functionality

--- FinalProject-IRobertson/javascript/athletic.js
+++ FinalProject-IRobertson/javascript/athletic.js
@@ -1,27 +1,73 @@
 // Athletic javascript file
 // by: Ian Robertson
 
-var milesRan = 0;
-var totalMiles = 0;
+var totalDistance = 0;
 var caloriesBurned = 0;
 var isPR = 0;   // PR (Personal Record)
+var voTwoMax = 0;
+var radioBtns = document.getElementsByClassName("radio_btn");
+radioBtns[0].checked = true; // auto checks default at start
 
-function getUserData()
+function validateInput()
 {
-    milesRan = document.getElementById("miles").value;
+    var form = document.getElementsByTagName("form")[0];
+    var inputFields = form.getElementsByTagName("input");
+    var isValid = false;
 
+    // checks if all form
+    if(isValid === true) {
+        calculateData();
+        outputData();
+    } else {
+        window.alert("Please enter all input fields!");
+    }
 }
 
-function calculateTotalMiles()
+function calculateData()
 {
+    // calculates based on what distance type is selected
+    if(radioBtns[0].checked === true) {
+        totalDistance = calculateMiles();
+    } else if(radioBtns[1].checked === true) {
+        totalDistance = calculateKilometers();
+    }
+}
 
+function calculateMiles()
+{
+    return 0;
+}
+
+function calculateKilometers()
+{
+    return 0;
 }
 
 function outputData()
 {
-    
+
+}
+
+function buttonEventListener()
+{
+    var submitForm = document.getElementById("submit-form");
+
+    if(submitForm.addEventListener) {
+        submitForm.addEventListener("click", validateInput, false);
+    } else if(submitForm.attachEvent) {
+        submitForm.attachEvent("onclick", validateInput, false);
+    }
 }
 
 // call functions
-getUserData();
-calculateTotalMiles();
+
+function createEventListeners()
+{
+    buttonEventListener();
+}
+
+if(window.addEventListener) {
+    window.addEventListener("load", createEventListeners, false);
+} else if(window.attachEvent) {
+    window.attachEvent("onload", createEventListeners);
+}
